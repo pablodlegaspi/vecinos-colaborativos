5ba6e72fb16715f9e9b93e20a41c32f3dfd51bed
FredesFL <fredesfl@gmail.com>
2019-08-08 22:07:28 -0300

    hice modelo post y su controlador

---
+++ vecinos-colaborativos/app/Http/Controllers/PostsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    //
+}
+
+// El timeline va a tener un formulario que por post trae la información para la función create 
